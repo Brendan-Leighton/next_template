91952742ea42479bd26dff7a30a299d2b742a61a
Brendan Leighton <br3ndan.l8n@gmail.com>
2026-09-10 11:14:19 -0400

    style: update ThemeToggler to match other nav links

--- src/components/theme-toggler.tsx
+++ src/components/theme-toggler.tsx
@@ -5,7 +5,7 @@ import { Moon, Sun } from 'lucide-react'
 import { useTheme } from '@teispace/next-themes'
 import { Button } from '@/components/shadcn/button'
 
-export function ThemeToggler() {
+export function ThemeToggler({ className }: { className?: string }) {
 
     const [mounted, setMounted] = useState(false)
     const { theme, setTheme } = useTheme()
@@ -23,10 +23,10 @@ export function ThemeToggler() {
         <Button
             variant='ghost'
             onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-            className='hover:text-background hover:bg-accent rounded'
+            className={className || 'hover:text-background hover:bg-accent rounded'}
         >
-            <Sun className='h-3xl w-3xl rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-            <Moon className='absolute h-3xl w-3xl rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+            <Sun className={theme === 'dark' ? 'hidden' : 'h-3xl w-3xl rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0'} />
+            <Moon className={theme === 'dark' ? 'h-3xl w-3xl rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' : 'hidden'} />
             <span className='sr-only'>Toggle theme</span>
         </Button>
     )
